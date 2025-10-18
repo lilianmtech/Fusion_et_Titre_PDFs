6b1fb4a75ad5a4353ceb63b8a13cef2e7f8c5e64
lilianmtech <97691880+lilianmtech@users.noreply.github.com>
2025-10-18 08:28:47 +0200

    Update Fusion_et_Titre_PDFs.py

--- Fusion_et_Titre_PDFs.py
+++ Fusion_et_Titre_PDFs.py
@@ -57,9 +57,15 @@ def main():
     if selection0 == "Par ordre d'importation":
         Info_ordre = "1ère import = 1er doc, 2ème import = 2ème doc, ..."
     elif selection0 == "Par ordre alphabétique":
-        Info_ordre = "En fonction du titre du PDF : 1)A15.pdf 2)G2.pdf 3)M9.pdf"
+        Info_ordre = '''En fonction du titre du PDF : 
+        1)A15.pdf 
+        2)G2.pdf 
+        3)M9.pdf'''
     elif selection0 == "Par ordre numérique":
-        Info_ordre = "En fonction du nombre présent dans le titre du PDF : 1)G2.pdf 2)M9.pdf 3)A15.pdf"
+        Info_ordre = '''En fonction du nombre présent dans le titre du PDF : 
+        1)G2.pdf 
+        2)M9.pdf 
+        3)A15.pdf'''
     st.info(Info_ordre,icon="ℹ")
     
     st.markdown("### ✍🏻Format")
@@ -103,7 +109,7 @@ def main():
     
     selection = Titre_1 + Increment + Titre_2
     
-    st.info(f"{selection}",icon="📟")
+    st.info(selection,icon="📟")
 
     
     st.markdown("### 📏Position")
@@ -178,6 +184,7 @@ def main():
 if __name__ == "__main__":
 
     main()
+
 
 
 
